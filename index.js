e40ac9713b6dbef82cf95c40d45bd611c4ee24e8
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-15 14:09:04 +0100

    Task 4
    - Replace the colors.json file with an api call through a XMLHttpRequest
    - Use the ReadyState property to track the request and the fire a callback function once the data has been received
    - Add "colors" as parameter for the insertColorsToDOM() and getSortedHexColorsArray()

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import { rgbToHex, sortColorsByBrightness } from './hexSortes.js';
 
-// Fetch the colors from colors.json file
-const response = await fetch('./colors.json');
-const colors = await response.json();
+const url = 'https://api.jsonbin.io/v3/b/6357a1c10e6a79321e33d472';
+const token = '$2b$10$82rC9Om26H9PYnm2CrxYQOi8.BqZA4R1EvU5xiXYMZ9LINy4zS0.6';
 
 // Convert rgba colors to hex and sort them based on brightness using the helper functions provided in hexSortes.js. Then return a new array with the sorted hex values.
-const getSortedHexColorsArray = () => {
+const getSortedHexColorsArray = (colors) => {
   const hexColors = [];
 
   for (const key in colors) {
@@ -20,9 +19,9 @@ const getSortedHexColorsArray = () => {
 };
 
 // Loop through the colors object and insert the individual colors into the DOM
-const insertColorsToDOM = () => {
+const insertColorsToDOM = (colors) => {
   const colorsContainer = document.querySelector('.colors-container');
-  const sortedHexColors = getSortedHexColorsArray();
+  const sortedHexColors = getSortedHexColorsArray(colors);
 
   for (const key in sortedHexColors) {
     colorsContainer.innerHTML += `
@@ -33,4 +32,32 @@ const insertColorsToDOM = () => {
   }
 };
 
-insertColorsToDOM();
+/* 
+  Resources used to learn more about XMLHttp 
+  - https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+  - https://alicemoretti.medium.com/xmlhttprequest-callbacks-and-promises-257a4e63fe9a
+*/
+const fetchColors = (callback) => {
+  let request = new XMLHttpRequest();
+
+  // Use the ReadyState property to track the request and then use a callback function once the data has been received.
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, JSON.parse(request.responseText).record);
+    } else if (request.readyState === 4) {
+      callback('Could not fetch data', undefined);
+    }
+  });
+
+  request.open('GET', url);
+  request.setRequestHeader('X-Access-Key', token);
+  request.send();
+};
+
+fetchColors((error, colors) => {
+  if (error) {
+    console.log(error);
+  } else {
+    insertColorsToDOM(colors);
+  }
+});
